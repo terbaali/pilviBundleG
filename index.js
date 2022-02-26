7406cdd89e9a442dc7dd6097015847f5a9e63ad5
Tero Vähäsarja <90072945+terbaali@users.noreply.github.com>
2022-02-26 15:26:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Pray for Ukraine');
+  res.send('Slava Ukraini!');
 })
 
 app.listen(port, () => {
